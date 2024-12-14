8ff06e405616211fbb09872cdea7b3b489848f5a
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-12-14 22:48:04 +0530

    A

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-const path = require('path');
 const { Server } = require('socket.io');
-const server = http.createServer(app);
-const io = new Server(server);  
+const http = require('http');
 
-io.on('connection', (socket) => {
+module.exports = (req, res) => {
+  // Create an HTTP server
+  const server = http.createServer((req, res) => {
+    res.status(200).send('Server running...');
+  });
+
+  // Initialize Socket.io
+  const io = new Server(server);
+
+  io.on('connection', (socket) => {
+    console.log('A user connected');
     socket.on('user-message', (data) => {
-        const selfData = { ...data, self: true };
-        socket.emit('message', selfData); 
-        socket.broadcast.emit('message', data); 
+      const selfData = { ...data, self: true };
+      socket.emit('message', selfData);
+      socket.broadcast.emit('message', data);
     });
-});
 
-app.use(express.static(path.join(__dirname, 'public')));
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+  // Start the server
+  server.listen(3000, () => {
+    console.log('Socket.IO server is running');
+  });
 
-server.listen(9000, () => console.log('Server started at PORT: 9000'));
+  return server; // Return server for Vercel to handle
+};
